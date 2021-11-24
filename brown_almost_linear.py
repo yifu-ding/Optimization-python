7931aedeb11772a739a697d4e51721a608ac1738
yfding <eveedyf@gmail.com>
2021-11-25 05:34:07 +0800

    question

---
+++ brown_almost_linear.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+
+class BrownAlmostLinear:
+    def __init__(self, n):
+        self.x_0 = np.array([1 / 2 for i in range(n)])
+
+        self.f_minimun_0 = 0
+        self.x_minimun_0 = np.ones_like(x_0)
+
+        self.f_minimun_1 = 1
+        self.x_minimun_1 = np.append(np.array([0 for i in range(n - 1)]),
+                                     n + 1)
+
+    def func(self, x):
+        n = x.shape[0]
+        m = n
+        sum = -n - 1 + np.sum(x)
+        tmp = 1
+        for j in range(m):
+            tmp *= x[j]
+        for i in range(n - 1):
+            f[i] = x[i] + sum
+        f[n - 1] = tmp - 1
+        return f
+
+    def grad(self, x):
+        n = x.shape[0]
+        g = np.zeros_like(x, dtype="float32")
+        tmp = 1
+        for l in range(n - 1):
+            g[l] = 1 + 1
+            tmp *= x[j]
+        g[n] = tmp
+        return g
--- brown_and_dennis.py
+++ brown_and_dennis.py
@@ -2,9 +2,27 @@ import numpy as np
 
 
 class BrownAndDennis:
-    def __init__(self):
+    def __init__(self, m=20):
         self.n = 4
+        self.m = m
+        assert self.m >= self.n
         self.x_0 = np.array([25, 5, -5, -1])
-        
+        self.f_minimun = 85822.2
+
     def func(self, x):
-    	
+        f = 0
+        for l in range(self.m):
+            t = l / 5
+            f = f + (x[0] + t * x[1] -
+                     np.exp(t))**2 + (x[2] + x[3] * np.sin(t) - np.cos(t))**2
+        return f
+
+    def grad(self, x):
+        g = np.zeros_like(x, dtype="float32")
+        for l in range(self.m):
+            t = l / 5
+            g[0] += 2 * (x[0] + t * x[1] - np.exp(t))
+            g[1] += 2 * (x[0] + t * x[1] - np.exp(t)) * t
+            g[2] += 2 * (x[2] + x[3] * np.sin(t) - np.cos(t))
+            g[3] += 2 * (x[2] + x[3] * np.sin(t) - np.cos(t)) * np.sin(t)
+        return g
