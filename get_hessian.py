8673632d1b1f6ed3891191d9c96a0c9ce18e342e
yfding <eveedyf@gmail.com>
2021-11-25 03:20:21 +0800

    get_hessian

---
+++ get_hessian.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+
+def get_hessian(func_name="extended_powell_singular"):
+    if func_name == "example":
+        G = np.array([[10, -9], [-9, 10]], dtype="float32")
+        return G + G.T
+    elif func_name == "extended_powell_singular":
+        return G
+    elif func_name == "":
+        return
+    elif func_name == "":
+        return
+    else:
+        raise NotImplementedError("未定义目标函数")
+    return G
