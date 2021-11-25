f5ce08314b930a5cfa36650a5a30db65d399a322
Yzx835 <46290866+Yzx835@users.noreply.github.com>
2021-11-26 00:38:54 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import numpy as np
 import logging
 import sys
+import time
 from jqxss import GD_algorithm  # exact line search
 from get_hessian import get_hessian
 from newton import NewtonMethod
@@ -53,6 +54,7 @@ question = BrownAndDennis(m=20)
 #                                           epsilon=1e-8,
 #                                           logger=logger)
 
+start_time = time.clock()
 total_iter, x_k, loss = NewtonMethod(start_point=question.x_0,
                                      func=question.func,
                                      grad=question.grad,
@@ -62,7 +64,7 @@ total_iter, x_k, loss = NewtonMethod(start_point=question.x_0,
                                      max_iters=1e3,
                                      method="hybrid wolfe simple",
                                      logger=logger)
-
+end_time = time.clock()
 # total_iter, x_k, loss = QuasiNewton(start_point=question.x_0,
 #                                     func=question.func,
 #                                     grad=question.grad,
@@ -78,4 +80,6 @@ logger.info("   迭代次数: " + str(total_iter))
 logger.info("   函数调用次数: " + str(question.call_f))
 logger.info("   迭代点的 x 值: " + str(x_k.reshape(1, -1)) + ", 函数值:" +
             str(question.func(x_k)))
-logger.info("   最优函数值:" + str(question.f_minimun))
+logger.info("   最优函数值: " + str(question.f_minimun))
+logger.info("   CPU时间（秒）: " + str(end_time-start_time))
+
