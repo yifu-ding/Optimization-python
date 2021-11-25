fc9b085f29f79fa8ba535123ee92a8e44b416486
Yzx835 <46290866+Yzx835@users.noreply.github.com>
2021-11-25 20:40:57 +0800

    update

--- brown_almost_linear.py
+++ brown_almost_linear.py
@@ -28,11 +28,17 @@ class BrownAlmostLinear:
     def grad(self, x):
         n = x.shape[0]
         g = np.zeros_like(x, dtype="float32")
+        sum = -n - 1 + np.sum(x)
         tmp = 1.
+        for j in range(n):
+            tmp *= x[j]
+        f_n = tmp - 1
         for l in range(n - 1):
-            g[l] = 1 + 1.
-            tmp *= x[l]
-        g[n - 1] = tmp
+            f = x[l] + sum
+            g = g + 2 * f
+            g[l] = g[l] + 2 * f
+            g[l] = g[l] + 2 * f_n * (tmp / x[l])
+        g[n-1] = g[n-1] + 2 * f_n * (tmp / x[n-1])
         return g
 
     def hessian(self, x):
@@ -43,7 +49,5 @@ class BrownAlmostLinear:
 if __name__ == '__main__':
     bal = BrownAlmostLinear(20)
     # x = bal.x_minimun_0
-    print(bal.func(bal.x_minimun_0))
-    print(bal.grad(bal.x_minimun_0))
-    print(bal.func(bal.x_minimun_1))
-    print(bal.grad(bal.x_minimun_1))
+    print(bal.func(bal.x_0))
+    print(bal.grad(bal.x_0))
